a6f05ce1d586e51ff53945eb6facce8d6729289a
Emmanuel-Youpy <emmydeji58@gmail.com>
2022-04-25 18:09:59 +0800

    first commit

---
+++ components/NFTCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const NFTCard = () => {
+  return (
+    <View>
+      <Text>NFTCard</Text>
+    </View>
+  );
+};
+
+export default NFTCard;
+
+const styles = StyleSheet.create({});
---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+const Home = () => {
+  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>;
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
